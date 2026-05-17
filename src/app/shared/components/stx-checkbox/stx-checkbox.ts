80878972a43aedf6dc3323028d3832468872298a
aliakseidrob <aliakseidrob@gmail.com>
2026-05-18 00:22:48 +0300

    feat: add checkbox template

--- src/app/shared/components/stx-checkbox/stx-checkbox.ts
+++ src/app/shared/components/stx-checkbox/stx-checkbox.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
 import { StxCheckboxLabelPosition } from './stx-checkbox.types';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-stx-checkbox',
-  imports: [],
+  imports: [MatCheckboxModule],
   templateUrl: './stx-checkbox.html',
   styleUrl: './stx-checkbox.scss',
 })
