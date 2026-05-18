63dcc79c25f379d1f1de52973e68cf150b5c3a81
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-05-18 16:12:01 +0300

    refactor: types for input to stx input component folder

--- src/app/shared/ui/stx-input/stx-input.ts
+++ src/app/shared/ui/stx-input/stx-input.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInput, MatSuffix } from '@angular/material/input';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
-import { InputAppearance, InputType } from '../../types/input';
+import { InputAppearance, InputType } from './stx-input.types';
 import { StxBaseControl } from '../../directives/stx-base-control';
 import { StxErrorMessagePipe } from '../../pipes/stx-error-message-pipe';
 import { ValidationMessages } from '../../types/validation';
