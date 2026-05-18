85e2582fc95cd0c06213f4b02d2ea67aed8301dc
aliakseidrob <aliakseidrob@gmail.com>
2026-05-18 22:06:13 +0300

    feat: add integration with forms

--- src/app/shared/components/stx-checkbox/stx-checkbox.ts
+++ src/app/shared/components/stx-checkbox/stx-checkbox.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, input, model } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, model, signal } from '@angular/core';
 import { StxCheckboxLabelPosition } from './stx-checkbox.types';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -9,14 +10,45 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   templateUrl: './stx-checkbox.html',
   styleUrl: './stx-checkbox.scss',
 })
-export class StxCheckbox {
+export class StxCheckbox implements ControlValueAccessor {
+  private readonly controlDir = inject(NgControl, { self: true, optional: true });
+
   readonly checked = model<boolean>(false);
 
-  readonly isDisabled = input<boolean>(false);
+  readonly isDisabledInput = input<boolean>(false);
   readonly isRequired = input<boolean>(false);
   readonly labelPosition = input<StxCheckboxLabelPosition>('after');
 
+  private readonly isFormDisabled = signal<boolean>(false);
+  protected readonly isDisabled = (): boolean => this.isDisabledInput() || this.isFormDisabled();
+
+  protected onChange: (value: boolean) => void = () => {};
+  protected onTouched: () => void = () => {};
+
+  constructor() {
+    if (this.controlDir) {
+      this.controlDir.valueAccessor = this;
+    }
+  }
+
   protected onValueChange(newValue: boolean): void {
     this.checked.set(newValue);
+    this.onChange(newValue);
+  }
+
+  writeValue(value: unknown): void {
+    this.checked.set(!!value);
+  }
+
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isFormDisabled.set(isDisabled);
   }
 }
