20f7d7b9fba5bd8520dd68cc0c3c4c942f4eb8a3
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-05-18 08:30:45 +0300

    feat: add stx base directive co reuse cva functionality

---
+++ src/app/shared/directives/stx-base-control.spec.ts
@@ -0,0 +1,103 @@
+// eslint-disable-next-line max-classes-per-file
+import { StxBaseControl } from './stx-base-control';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForMicrotasks } from '../utils/wait-for-microtask';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'stx-child-input',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `<input [formControl]="control" />`,
+})
+class ChildInputComponent extends StxBaseControl<string> implements OnInit {
+  override ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  protected override formatValue(value: string | null): string | null {
+    return value ? value.toUpperCase() : value;
+  }
+}
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [ReactiveFormsModule, ChildInputComponent],
+  template: `<stx-child-input [formControl]="parentControl" />`,
+})
+class ParentComponent {
+  parentControl = new FormControl<string | null>('');
+}
+
+describe('StxBaseControl', () => {
+  let fixture: ComponentFixture<ParentComponent>;
+  let parentInstance: ParentComponent;
+  let childInstance: ChildInputComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, ChildInputComponent, ParentComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ParentComponent);
+    parentInstance = fixture.componentInstance;
+    const debugEl = fixture.debugElement.children[0];
+    childInstance = debugEl.componentInstance as ChildInputComponent;
+    fixture.detectChanges();
+  });
+
+  it('should create an instance and implement CVA', () => {
+    expect(childInstance).toBeTruthy();
+    expect(childInstance.control).toBeTruthy();
+  });
+
+  it('should synchronize writing value from parent control to child', async () => {
+    const testWord = 'crypto';
+
+    parentInstance.parentControl.setValue(testWord);
+
+    await waitForMicrotasks();
+    fixture.detectChanges();
+
+    expect(childInstance.control.value).toBe(testWord.toUpperCase());
+  });
+
+  it('should synchronize writing value from child control to parent', async () => {
+    const testWord = 'crypto';
+
+    childInstance.control.setValue(testWord);
+
+    await waitForMicrotasks();
+    fixture.detectChanges();
+
+    expect(parentInstance.parentControl.value).toBe(testWord.toUpperCase());
+  });
+
+  it('should synchronize errors and validation from parent', async () => {
+    parentInstance.parentControl.setValidators([Validators.required]);
+    parentInstance.parentControl.setValue('');
+
+    parentInstance.parentControl.updateValueAndValidity();
+
+    await waitForMicrotasks();
+    fixture.detectChanges();
+
+    expect(childInstance.control.invalid).toBeTruthy();
+    expect(childInstance.control.hasError('required')).toBeTruthy();
+  });
+
+  it('should mark child control as touched, if parent control become touched', async () => {
+    expect(childInstance.control.touched).toBe(false);
+
+    parentInstance.parentControl.markAsTouched();
+    parentInstance.parentControl.updateValueAndValidity();
+
+    await waitForMicrotasks();
+    fixture.detectChanges();
+
+    expect(childInstance.control.touched).toBe(true);
+  });
+});
---
+++ src/app/shared/directives/stx-base-control.ts
@@ -0,0 +1,88 @@
+import { DestroyRef, Directive, inject, OnInit, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { distinctUntilChanged, map } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Directive()
+export class StxBaseControl<T> implements ControlValueAccessor, OnInit {
+  protected readonly destroyRef = inject(DestroyRef);
+  @Optional() @Self() protected readonly ngControl = inject(NgControl, { optional: true });
+
+  readonly control = new FormControl<T | null>(null);
+
+  protected onChange: (value: T | null) => void = () => {};
+  protected onTouched: () => void = () => {};
+
+  constructor() {
+    if (this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
+
+  ngOnInit(): void {
+    this.control.valueChanges
+      .pipe(
+        map(val => this.formatValue(val)),
+        distinctUntilChanged(),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(formattedValue => {
+        if (this.control.value !== formattedValue) {
+          this.control.setValue(formattedValue, { emitEvent: false });
+        }
+        this.onChange(formattedValue);
+      });
+
+    if (this.ngControl?.control) {
+      const parentControl = this.ngControl.control;
+
+      this.control.setValidators(parentControl.validator);
+      this.control.setAsyncValidators(parentControl.asyncValidator);
+      this.control.updateValueAndValidity({ emitEvent: false });
+
+      let wasTouched = false;
+      parentControl.statusChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+        if (this.control.errors !== parentControl.errors) {
+          this.control.setErrors(parentControl.errors);
+        }
+
+        if (parentControl.touched && !wasTouched) {
+          this.control.markAsTouched();
+          wasTouched = true;
+        } else if (!parentControl.touched && wasTouched) {
+          this.control.markAsUntouched();
+          wasTouched = false;
+        }
+      });
+    }
+  }
+
+  protected formatValue(value: T | null): T | null {
+    return value;
+  }
+
+  writeValue(value: T | null): void {
+    const formattedValue = this.formatValue(value);
+
+    if (this.control.value === formattedValue) return;
+
+    this.control.setValue(formattedValue, { emitEvent: false });
+    this.control.updateValueAndValidity({ emitEvent: false });
+  }
+
+  registerOnChange(fn: (value: T | null) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.control.disable({ emitEvent: false });
+    } else {
+      this.control.enable({ emitEvent: false });
+    }
+  }
+}
