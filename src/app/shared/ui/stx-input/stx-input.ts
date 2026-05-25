2b3a77cfdbac5142a503b09717f1452e79683bac
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-05-25 20:21:24 +0300

    feat: add possibility to set action on click icon and hide and show value like a secret ket

--- src/app/shared/ui/stx-input/stx-input.ts
+++ src/app/shared/ui/stx-input/stx-input.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output, signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInput, MatSuffix } from '@angular/material/input';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { InputAppearance, InputType } from './stx-input.types';
 import { StxBaseControl } from '../../directives/stx-base-control';
 import { StxErrorMessagePipe } from '../../pipes/stx-error-message-pipe';
+import { ValidationMessages } from '../../types/validation';
 import { MatIcon } from '@angular/material/icon';
 import { MaterialIcons } from 'material-design-icons-literal-types';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { StxDefaultErrorStateMatcher } from './default-error-state-matcher';
 
 @Component({
   selector: 'stx-input',
@@ -23,6 +26,29 @@ export class StxInput<T = string> extends StxBaseControl<T> {
   readonly label = input<string>();
   readonly placeholder = input<string>('');
   readonly icon = input<MaterialIcons | ''>('');
+  readonly defaultErrorStateMatcher = new StxDefaultErrorStateMatcher();
+  readonly customErrorStateMatcher = input<ErrorStateMatcher>();
+  readonly secret = input<boolean>(false);
+  readonly secretVisibility = signal<boolean>(false);
+
+  readonly typeHandler = computed(() => {
+    if (this.secret()) {
+      return this.secretVisibility() ? 'text' : this.type();
+    }
+
+    return this.type();
+  });
+
+  readonly iconHandler = computed((): MaterialIcons | '' => {
+    if (this.secret()) {
+      return this.secretVisibility() ? 'visibility' : 'visibility_off';
+    }
+
+    return this.icon();
+  });
+  readonly iconClick = output<void>();
+
+  readonly customErrors = input<ValidationMessages>({});
 
   readonly appearance = input<InputAppearance>('outline');
   readonly trimmed = input<boolean>(false);
@@ -30,5 +56,15 @@ export class StxInput<T = string> extends StxBaseControl<T> {
   protected override formatValue(v: T | null): T | null {
     const value = this.trimmed() && typeof v === 'string' ? v.trim() : v;
     return value as T | null;
+  }
+
+  onIconClick(event: MouseEvent): void {
+    event.stopPropagation();
+
+    if (this.secret()) {
+      this.secretVisibility.update(v => !v);
+    }
+
+    this.iconClick.emit();
   }
 }
