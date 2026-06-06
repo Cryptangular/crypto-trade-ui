19a50cdd294ebaf944ddf29f7eb6e626d4946dfe
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-06 05:21:10 +0300

    feat: improve work with signal in template by varaible, add check for validation menu item by id

--- src/app/shared/ui/stx-menu-item/stx-menu-item.ts
+++ src/app/shared/ui/stx-menu-item/stx-menu-item.ts
@@ -1,15 +1,19 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { MenuItem } from '../stx-three-dot-menu/stx-three-dot-menu.types';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatIcon } from '@angular/material/icon';
+import { MenuItem, ParentMenuItem } from '../stx-three-dot-menu/stx-three-dot-menu.types';
 
 @Component({
   selector: 'stx-menu-item',
-  imports: [MatMenu, MatMenuItem, MatMenuTrigger, MatIcon],
+  imports: [MatMenu, MatMenuItem, MatMenuTrigger, MatIcon, StxMenuItem],
   templateUrl: './stx-menu-item.html',
   styleUrl: './stx-menu-item.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StxMenuItem {
   readonly item = input.required<MenuItem>();
+
+  protected isParent(item: MenuItem): item is ParentMenuItem {
+    return item.kind === 'parent';
+  }
 }
