20d68350f25ebb81e108f097cbfddf392cb5906b
aliakseidrob <aliakseidrob@gmail.com>
2026-06-18 01:36:14 +0300

    feat: add angular material

--- src/app/pages/markets-page/markets-page.ts
+++ src/app/pages/markets-page/markets-page.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CryptoMarketService } from './crypto-market.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'stx-markets-page',
-  imports: [],
+  standalone: true,
+  imports: [MatTableModule],
   templateUrl: './markets-page.html',
   styleUrl: './markets-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,6 +14,8 @@ export class MarketsPage {
   private readonly marketService = inject(CryptoMarketService);
 
   readonly tokens = this.marketService.allTokens;
+
+  readonly displayedColumns: string[] = ['coin', 'price', 'change24h', 'volume'];
 
   constructor() {
     this.marketService.loadInitialMarketData();
