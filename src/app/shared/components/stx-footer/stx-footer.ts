cf0c6326a7d97fb00e12a19427df5ca47cf56dd9
aliakseidrob <aliakseidrob@gmail.com>
2026-06-17 21:05:23 +0300

    feat: implement footer

--- src/app/shared/components/stx-footer/stx-footer.ts
+++ src/app/shared/components/stx-footer/stx-footer.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'stx-footer',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './stx-footer.html',
   styleUrl: './stx-footer.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StxFooter {}
+export class StxFooter {
+  readonly currentYear = 2026;
+  readonly company = 'Stonks';
+  readonly rsSchoolLink = 'https://rs.school';
+}
