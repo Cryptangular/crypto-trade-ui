631efb5445a7c311a75488d0070550ff5deb3a3c
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-06-21 08:15:45 +0300

    feat: add settings service

--- src/app/pages/stx-settings-page/services/stx-settings.ts
+++ src/app/pages/stx-settings-page/services/stx-settings.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { SettingsRequest, SettingsResponse, SettingsResponseDTO } from '../types/settings';
+import { SETTINGS_ENDPOINT } from '../constants/settings.constants';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StxSettings {}
+export class StxSettings {
+  private readonly httpClient = inject(HttpClient);
+  private readonly prefix = SETTINGS_ENDPOINT;
+  private readonly url = `${environment.apiUrl}/${this.prefix}`;
+
+  getSettings(): Observable<SettingsResponse<SettingsResponseDTO>> {
+    return this.httpClient.get<SettingsResponse<SettingsResponseDTO>>(this.url, { withCredentials: true });
+  }
+
+  setSettings(body: SettingsRequest): Observable<SettingsResponse<SettingsResponseDTO>> {
+    return this.httpClient.post<SettingsResponse<SettingsResponseDTO>>(this.url, body, { withCredentials: true });
+  }
+
+  removeSettings(): Observable<SettingsResponse<null>> {
+    return this.httpClient.delete<SettingsResponse<null>>(this.url, { withCredentials: true });
+  }
+}
