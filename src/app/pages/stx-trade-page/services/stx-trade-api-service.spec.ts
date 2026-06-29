951c689bdf38fdb6c88aecf5df53f8c55a65aec4
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-06-29 23:35:33 +0400

    test(): mock trade api services in trade page spec

--- src/app/pages/stx-trade-page/services/stx-trade-api-service.spec.ts
+++ src/app/pages/stx-trade-page/services/stx-trade-api-service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Observable, of } from 'rxjs';
 import { StxTradeApiService } from './stx-trade-api-service';
+import { StxApiService } from '../../../../core/services/stx-api/stx-api-service';
 
 describe('StxTradeApiService', () => {
   let service: StxTradeApiService;
 
+  const stxApiServiceMock = {
+    get: (): Observable<unknown> => of([]),
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [StxTradeApiService, { provide: StxApiService, useValue: stxApiServiceMock }],
+    });
+
     service = TestBed.inject(StxTradeApiService);
   });
 
