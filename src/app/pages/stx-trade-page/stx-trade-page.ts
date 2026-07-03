c84dbf1f84f7b3a6c58767fd9e089dad5ad9b1b0
Alexandr Zhdanko <lex.zhdanko@gmail.com>
2026-07-03 23:32:39 +0400

    feat: implement multiplex binance websocket integration

--- src/app/pages/stx-trade-page/stx-trade-page.ts
+++ src/app/pages/stx-trade-page/stx-trade-page.ts
@@ -1,7 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { WebSocketMessage, WebSocketService } from './services/stx-trade-ws.service';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
 import { StxPriceChart } from './components/stx-price-chart/stx-price-chart';
 import { StxTradeApiService } from './services/stx-trade-api-service';
 import { StxTradePageHeader } from './components/stx-trade-page-header/stx-trade-page-header';
+import { CandlestickData } from './models/stx-trade-model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+export type KlineData = {
+  type: string;
+  time: number;
+  symbol: string;
+  kline: CandlestickData;
+};
 
 @Component({
   selector: 'stx-trade-page',
@@ -11,4 +21,48 @@ import { StxTradePageHeader } from './components/stx-trade-page-header/stx-trade
   styleUrl: './stx-trade-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StxTradePage {}
+export class StxTradePage implements OnInit, OnDestroy {
+  private wsService = inject(WebSocketService);
+  isConnected = false;
+  readonly data = signal(null);
+  readonly klineData: WritableSignal<KlineData | undefined> = signal(undefined);
+  readonly priceChange: WritableSignal<number> = signal(0);
+
+  ngOnInit(): void {
+    this.wsService.connect('wss://stream.binance.com:9443/ws/btcusdt@kline_1m');
+
+    this.wsService.connectionStatus$.pipe().subscribe(status => (this.isConnected = status));
+
+    this.wsService.subscribeToStreams(['btcusdt@kline_1m', 'btcusdt@ticker']);
+  }
+
+  constructor() {
+    const ws = this.wsService;
+    ws.messages$.pipe(takeUntilDestroyed()).subscribe({
+      next: (m: WebSocketMessage) => {
+        if (m.e === 'kline') {
+          const formattedKline = {
+            type: m.e,
+            time: m.E,
+            symbol: m.s,
+            kline: {
+              time: m['k']['t'],
+              open: m['k']['o'],
+              high: m['k']['h'],
+              low: m['k']['l'],
+              close: m['k']['c'],
+            },
+          };
+          this.klineData.set(formattedKline);
+        } else if (m.e === '24hrTicker') {
+          const price = m['c'];
+          this.priceChange.set(price);
+        }
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.wsService.disconnect();
+  }
+}
