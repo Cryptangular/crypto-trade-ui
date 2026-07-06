0ce6a442551bb5aff6ce622320b3c73886803957
aliakseidrob <aliakseidrob@gmail.com>
2026-07-06 13:07:30 +0300

    feat: add loading overlay service

--- src/app/shared/components/stx-loading-overlay/stx-loading-overlay.ts
+++ src/app/shared/components/stx-loading-overlay/stx-loading-overlay.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { LoadingOverlayService } from '../../../../core/services/stx-loading-overlay/loading-overlay.service';
 
 @Component({
   selector: 'stx-loading-overlay',
   imports: [],
   templateUrl: './stx-loading-overlay.html',
   styleUrl: './stx-loading-overlay.scss',
+  host: {
+    '[hidden]': '!loadingService.loading()',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StxLoadingOverlay {}
+export class StxLoadingOverlay {
+  protected readonly loadingService = inject(LoadingOverlayService);
+}
