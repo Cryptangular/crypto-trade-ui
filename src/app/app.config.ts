14b5e8597400bb7e15b7b4a881ebfe6ef5787e74
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-07-06 20:44:54 +0300

    feat: add app initializer
    # Conflicts:
    #	src/app/app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { AuthService } from './pages/auth/services/auth-service';
+import { firstValueFrom } from 'rxjs';
+import { StxSettingsService } from './pages/stx-settings-page/services/stx-settings.service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loadingInterceptor } from '../core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAppInitializer(async () => {
+      const authService = inject(AuthService);
+      const settingsService = inject(StxSettingsService);
+
+      await firstValueFrom(settingsService.getConnectionStatus());
+      await firstValueFrom(authService.checkAuth());
+    }),
     provideHttpClient(withInterceptors([loadingInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
