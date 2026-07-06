50ce4ebaeb09901f491c498dd78be613266a1bc1
aliakseidrob <aliakseidrob@gmail.com>
2026-07-06 13:38:13 +0300

    feat: add debounce and min-show time to loading interceptor

--- src/core/interceptors/loading.interceptor.ts
+++ src/core/interceptors/loading.interceptor.ts
@@ -5,11 +5,36 @@ import { LoadingOverlayService } from '../services/stx-loading-overlay/loading-o
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoadingOverlayService);
-  loadingService.show();
+
+  const DEBOUNCE_TIME = 300;
+  const MIN_SHOW_TIME = 300;
+
+  let isLoaderShown = false;
+  let showStartTime: number | null = null;
+
+  const debounceTimer = setTimeout(() => {
+    loadingService.show();
+    isLoaderShown = true;
+    showStartTime = Date.now();
+  }, DEBOUNCE_TIME);
 
   return next(req).pipe(
     finalize(() => {
-      loadingService.hide();
+      clearTimeout(debounceTimer);
+
+      if (isLoaderShown && showStartTime) {
+        const elapsed = Date.now() - showStartTime;
+
+        const remainingTime = Math.max(0, MIN_SHOW_TIME - elapsed);
+
+        if (remainingTime > 0) {
+          setTimeout(() => {
+            loadingService.hide();
+          }, remainingTime);
+        } else {
+          loadingService.hide();
+        }
+      }
     })
   );
 };
