a9c737d3c2f7f7b7641359601fe8c582a13e1e20
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-07-06 20:50:42 +0300

    feat: add settings messages for connection with binance

--- src/app/pages/stx-settings-page/constants/settings.constants.ts
+++ src/app/pages/stx-settings-page/constants/settings.constants.ts
@@ -4,6 +4,8 @@ export const SETTINGS_MESSAGES = {
   SETTINGS_REMOVED: 'Settings were removed!',
   SETTINGS_SETTED: 'Secret and Api keys were setted!',
   SOMETHING_WENT_WRONG: 'Something went wrong',
+  CONNECTION_ON: 'CONNECTION_ON',
+  CONNECTION_OFF: 'CONNECTION_OFF',
 } as const;
 
 export const SETTINGS_ERROR_MESSAGES = {
