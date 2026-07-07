a50205fa96d050be1f23ed1d58887a5be62d2f25
AnatoliRub <59088353+AnatoliRub@users.noreply.github.com>
2026-07-07 18:08:36 +0300

    fix: rename settings guard

--- src/app/pages/stx-settings-page/guards/settings.guard.spec.ts
+++ src/app/pages/stx-settings-page/guards/settings.guard.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { settingsGuard } from './settings-guard';
+import { settingsGuard } from './settings.guard';
 
 describe('settingsGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
